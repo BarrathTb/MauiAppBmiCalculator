6686b789baae326c6a4fa4134ce539f42909cf09
thomasbarrath <thomas.barrath@lanex.com>
2023-11-08 12:49:26 -0600

    Add project files.

---
+++ MauiAppBmi/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace MauiAppBmi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            double weightInLbs = Convert.ToDouble(In_Weight.Text);
+            double heightInInches = Convert.ToDouble(In_Height.Text);
+
+
+            double bmi = ((weightInLbs / (heightInInches * heightInInches)) * 703);
+            Out_Text.Text = bmi.ToString();
+        }
+
+
+
+    }
+}
